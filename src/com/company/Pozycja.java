897cede1f6bf92e9f3dadb2450bd6ded3483e214
Daniel Kaczorowski <kaczorowski.daniel@gmail.com>
2016-02-29 09:51:43 +0100

    ver, 0.03

--- src/com/company/Pozycja.java
+++ src/com/company/Pozycja.java
@@ -4,9 +4,9 @@ package com.company;
  * Created by user on 2016-02-28.
  */
 public class Pozycja {
-    private String nazwaTowaru;
-    private int ileSztuk;
-    private double cena;
+    protected String nazwaTowaru;
+    protected int ileSztuk;
+    protected double cena;
 
     public Pozycja(String nazwaTowaru, int ileSztuk, double cena) {
         this.nazwaTowaru = nazwaTowaru;
@@ -19,7 +19,7 @@ public class Pozycja {
 
     @Override
     public String toString() {
-        String lancuch = String.format("%s%20s%2s%4s%2s%10s%2s",nazwaTowaru,cena,"zl",ileSztuk,"szt.",cena*ileSztuk,"zl");
+        String lancuch = String.format("%s%20s%2s%4s%2s%10s%2s",nazwaTowaru,cena,"zl",ileSztuk,"szt.",obliczWartosc(),"zl");
         return lancuch;
     }
 }
--- src/com/company/Zamowienie.java
+++ src/com/company/Zamowienie.java
@@ -7,26 +7,42 @@ import java.util.Arrays;
  */
 public class Zamowienie {
     Pozycja[] pozycje;
-    int ileDodanych;
-    int maksRozmiar;
+    private int ileDodanych;
+    private int maksRozmiar;
 
     public Zamowienie() {
+        this.maksRozmiar=10;
     }
 
     public Zamowienie(int maksRozmiar) {
         this.maksRozmiar = maksRozmiar;
     }
     public void dodajPozycje(Pozycja p){
+        if(ileDodanych>=maksRozmiar){
+            System.out.println("Limit pozycji wykorzystany.");
+        }
+        else{
+            pozycje[ileDodanych] = new Pozycja(p.nazwaTowaru, p.ileSztuk, p.cena);
+        }
+        ileDodanych++;
     }
     public double obliczWartosc(){
         double wartosc=0;
+        for(int i = 0; i < ileDodanych; i++)
+        {
+            wartosc += pozycje[i].obliczWartosc();
+        }
         return wartosc;
     }
 
     @Override
     public String toString() {
-        return "Zamowienie{" +
-                "pozycje=" + Arrays.toString(pozycje) +
-                '}';
+        String str = "Zamówienie:\n";
+        for(int i = 0; i < ileDodanych; i++)
+        {
+            str += pozycje[i] + "\n";
+        }
+        str += "\nRazem: " + obliczWartosc() + " zł";
+        return str;
     }
 }
